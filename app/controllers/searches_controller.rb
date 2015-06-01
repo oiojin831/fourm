444600069119e60c3a33ffb3aeef9cd469abbfd6
eungjinlee <oiojin831@gmail.com>
2015-06-01 22:54:43 +0000

    Add recent photos of searched user

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,8 @@
 class SearchesController < ApplicationController
   def index
     search = params[:search]
-    @instagram = Instagram.user_search(search, {:count => 1})
+    user = Instagram.user_search(search, {count: 1})
+    user_id = user[0].id
+    @instagram = Instagram.user_recent_media(user_id, {count: 10})
   end
 end
